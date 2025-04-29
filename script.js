24b4b59049666ca3d0f7372edd1fed7c2d3f86cf
mSev7n <pythagorasm7@gmail.com>
2025-04-29 17:07:50 +0100

    added an index.html and linked it to a style and script file ...then added two div tags in the index.html file, styled and scripted it to make 256 grids

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("script loaded")
+
+const container = document.querySelector(".container")
+
+for (let i = 0; i < 256; i++)
+{
+    const square =  document.createElement("div")
+    square.classList.add("square")
+    container.appendChild(square)   
+}
